19ef55f284a275457bde25f8066b1f37cdd03c08
hanjeonghyun <limithyunii@gmail.com>
2023-12-27 00:18:47 +0900

    [Feat] Signup_1,2 Login 페이지

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Signup1 from "./Components/signup_1";
+import Signup2 from "./Components/signup_2";
+import Login from "./Components/Login";
+
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
+      <BrowserRouter>
+        <Routes>
+          {
+            <Route
+              path='/Login'
+              element={<Login />}
+            />
+          }
+          <Route
+            path='/signup_1'
+            element={<Signup1 />}
+          />
+          {
+            <Route
+              path='/signup_2'
+              element={<Signup2 />}
+            />
+          }
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
